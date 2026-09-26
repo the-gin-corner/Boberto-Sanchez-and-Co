2398663bf0407c18364fbdbb6162276138ba9351
Wynn <meltingpoint56@outlook.com>
2026-09-26 00:40:36 -0700

    Refining camera and adding music/jumping sfx

--- scripts/boberto.gd
+++ scripts/boberto.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sfx: AudioStreamPlayer2D = $"jump sfx"
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
@@ -15,6 +16,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sfx.play()
 		
 
 	# Get the input direction and handle the movement/deceleration.
--- scripts/main_level.gd
+++ scripts/main_level.gd
@@ -6,6 +6,18 @@ extends Node2D
 @onready var charDialogue: Label = $Camera2D/Dialogue
 @onready var charName: Label = $Camera2D/Name
 
+@onready var correctArrangement = {
+	"Lower Left": "Keno",
+	"Lower Middle": "Kalimba",
+	"Lower Right": "Tabs",
+	"Upper Left": "Piri",
+	"Upper Right": "Miro"
+}
+
+var cameraWidth
+
+func _ready() -> void:
+	cameraWidth = 1200
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
@@ -19,34 +31,8 @@ func _unhandled_input(event: InputEvent) -> void:
 			
 			#print("{0}: {1}".format([friend.name, friend.collisionState]))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var bobertoPos = boberto.position.x
-	
-	#Camera switching between screens
-	#-------
-	#Surely there is a better way to do this lol
-	if bobertoPos < 1200:
-		camera_2d.position.x = 0
-	else:
-		if bobertoPos > 1200 && bobertoPos < 2400:
-			camera_2d.position.x = 1200
-		if bobertoPos > 2400 && bobertoPos < 3600:
-			camera_2d.position.x = 2400
-		if bobertoPos > 3600 && bobertoPos < 4800:
-			camera_2d.position.x = 3600
-		if bobertoPos > 4800 && bobertoPos < 6000:
-			camera_2d.position.x = 4800
-		if bobertoPos > 6000 && bobertoPos < 7200:
-			camera_2d.position.x = 6000
-		if bobertoPos > 7200 && bobertoPos < 8400:
-			camera_2d.position.x = 7200
-		if bobertoPos > 8400 && bobertoPos < 9600:
-			camera_2d.position.x = 8400
-		if bobertoPos > 9600 && bobertoPos < 10800:
-			camera_2d.position.x = 9600
-		if bobertoPos > 10800:
-			camera_2d.position.x = 10800
 	
 	#Handling NPC sprite flipping
 	for friend in get_tree().get_nodes_in_group("friends"):
@@ -57,7 +43,21 @@ func _process(delta: float) -> void:
 		else:
 			sprite.flip_h = false
 	
+	switchRooms()
+	
 
+#Credit to DevDrache on YouTube for the mini tutorial on this
+func switchRooms():
+	var bobertoPos = boberto.global_position.x
+	var cameraPos = camera_2d.global_position.x
+	
+	var right = bobertoPos > cameraPos + cameraWidth
+	var left = bobertoPos < cameraPos
+	
+	if right:
+		camera_2d.global_position.x += cameraWidth
+	elif left:
+		camera_2d.global_position.x -= cameraWidth
 
 func _on_timer_timeout() -> void:
 	charName.text = ""
